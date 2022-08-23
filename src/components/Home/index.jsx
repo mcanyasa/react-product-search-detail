8cff000015e780e3498d569770c6afa1e81e459e
Mustafa Can Yasa <mcanyasa@hotmail.com>
2022-08-23 13:20:30 +0300

    webpack config

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/* import reactLogo from 'images/react.png'; */
+
+
+class Home extends React.Component { 
+   render() {
+      return (
+         <div>
+           
+		   <h2>Welcome  </h2>
+		   
+		   <br />
+
+         </div>
+      );
+   }
+}
+
+export default Home;
---
+++ src/components/ProductDetail/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class ProductDetail extends React.Component { 
+   render() {
+      return (
+         <div>
+           
+		       <h2>ProductDetail</h2><br />
+  
+               
+            			 
+
+         </div>
+      );
+   }
+}
+
+export default ProductDetail;
